b537e6c4817fe1d1b2093b56b96665eb734c4c93
William <will@ministrycentered.com>
2020-05-13 08:45:16 -0700

    change to text

--- src/pages/about.js
+++ src/pages/about.js
@@ -80,19 +80,13 @@ const About = () => (
         <h4 
         style={{
           marginTop: `4em`,
-          marginBottom: `1em`,
+          marginBottom: `3em`,
         }}>
           Love defeats fear
         </h4>
         <p>The COVID-19 pandemic has turned everyones life upside down in some way. However, there are some of us that have been more affected than others. Whether it's the loss of life, or financial struggle, there are people out there that can use our help.</p>
         <p>Our ethos is rooted in our faith. We believe that we are called to help all who are in need regardless of faith, race, creed, or politics.</p>
-        <p>One of the greatest verses in the Bible to us, is <br/>
-        <strong>1 John 4:18</strong><br/>
-        <em>"There is no fear in love; but perfect love casts out fear, because fear involves torment. But he who fears has not been made perfect in love."</em>
-        </p>
-        <p>God sent his Son into the world not to condem it, but to save it. He came to spread the love of God and did not limit that to only certain groups of people, but to all of us.</p>
-        <p>Unfortunately, there are many that use the name of God for their own personal gain or agenda, and we are committed to turning that around.</p>
-        <p>Even though we are a Christian based organization, we work with people of all faiths and beliefs that promote a positive outlook and encourage the betterment of mankind without causing harm. We are commiting to using our skill sets to help the community during times of striff or hardship.</p>
+        <p>We work with people of all faiths and beliefs that promote a positive outlook and encourage the betterment of mankind without causing harm. We are commiting to using our skill sets to help the community during times of striff or hardship.</p>
         <br/>
         <br/>
         <br/>
