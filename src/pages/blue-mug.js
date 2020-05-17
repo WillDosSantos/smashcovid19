ca4284ad779158f746b139d7dedc03f8e68350b1
William <will@ministrycentered.com>
2020-05-17 00:23:39 -0700

    blue mug

--- src/pages/blue-mug.js
+++ src/pages/blue-mug.js
@@ -43,9 +43,9 @@ const FifthPage = () => (
           <img className="push-left" src={bluemug4} alt="Blue Mug Restaurant" />
           <p>When it comes to Blue Mug Coffee and Tea, they really do put love into what they do. It has been very inspiring to see this little coffee shop be such a beckon of hope for their community.</p>  
           <p>With all that is going on with the COVID-19 lockdowns, and the fear from the virus itself, they still push forward in keeping their business alive and serving their city with great coffee and tea to bring you that comfort of home and what we have to look foward to when this is all over.</p>
-          <p>Blug Mug is a great way to start your morning. Wake up, and swing by the shop for your to-go order, and treat yourself to a relaxing break, rather than reveling in things that are outside of your control.</p>
+          <p>Blue Mug is a great way to start your morning. Wake up, and swing by the shop for your to-go order, and treat yourself to a relaxing break, rather than reveling in things that are outside of your control.</p>
           <p>And if you aren't in the area, you can still help this great establishment by purchasing this t-shirt. As always all earnings go to the business we support.</p>
-          <p>Blug Mug has a wide variety of to-go drinks which you can <a href="http://www.blue-mug.com/menu/">view here</a>.</p>
+          <p>Blue Mug has a wide variety of to-go drinks which you can <a href="http://www.blue-mug.com/menu/">view here</a>.</p>
           <img className="push-right img-small" src={bluemug1} alt="Tasty Treats" />
           <p>In addition to their drinks, Blue Mug also offers a variety of delicious pastries and snacks for you to enjoy.</p>
           <p>I mean, who wouldn't enjoy a Donut, Crumb Cake or nice Danish. And Cinnamon Rolls...let's not forget about the Cinnamon Rolls. So in addition to a great beverage, treat yourself to some good food.</p>
@@ -54,9 +54,9 @@ const FifthPage = () => (
          <h2 className="mt-1">The People</h2>
           <hr/>
           <img className="push-left img-small" src={bluemug3} alt="Blue Mug Crew" />
-          <p>Blug Mug Coffe and Tea is owned by Stephanie Shumate. Along with her small crew, Stephanie serves the people of Escondido with great tasting drinks and a friendly atmosphere.</p>
-          <p>The people of Blug Mug also support local community charities including and especially local law enforcement.</p>
-          <p>To find out more about Blug Mug, check out their social media on <a href="https://www.facebook.com/BlueMugCoffeeTea/">Facebook</a>, <a href="https://www.instagram.com/bluemugcoffeeandtea/">Instagram</a>, and <a href="https://www.yelp.com/biz/blue-mug-coffee-and-tea-escondido-2">Yelp</a></p>
+          <p>Blue Mug Coffe and Tea is owned by Stephanie Shumate. Along with her small crew, Stephanie serves the people of Escondido with great tasting drinks and a friendly atmosphere.</p>
+          <p>The people of Blue Mug also support local community charities including and especially local law enforcement.</p>
+          <p>To find out more about Blue Mug, check out their social media on <a href="https://www.facebook.com/BlueMugCoffeeTea/">Facebook</a>, <a href="https://www.instagram.com/bluemugcoffeeandtea/">Instagram</a>, and <a href="https://www.yelp.com/biz/blue-mug-coffee-and-tea-escondido-2">Yelp</a></p>
           </article>
         <article>
           <h2 style={{
